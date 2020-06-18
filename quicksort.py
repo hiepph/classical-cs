e22a934dcfbd39830d6eab13772c5835122f0f39
Hiep <hiepph@localhost.localdomain>
2020-06-18 23:47:12 +0700

    feat: 3-way partition quicksort

--- quicksort.py
+++ quicksort.py
@@ -9,20 +9,28 @@ def QuickSort(A, l, r):
     k = random.randint(l, r)
     A[l], A[k] = A[k], A[l]
 
-    m = Partition(A, l, r)
-    QuickSort(A, l, m - 1)
-    QuickSort(A, m + 1, r)
+    m1, m2 = Partition3(A, l, r)
+    QuickSort(A, l, m1 - 1)
+    QuickSort(A, m2 + 1, r)
 
 
-def Partition(A, l, r):
+def Partition3(A, l, r):
     p = A[l]
-    j = l
-    for i in range(l + 1, r + 1):
-        if A[i] <= p:
-            j = j + 1
-            A[i], A[j] = A[j], A[i]
-    A[l], A[j] = A[j], A[l]
-    return j
+    m1 = l
+    m2 = r
+    i = l
+    while i <= m2:
+        if A[i] < p:
+            A[m1], A[i] = A[i], A[m1]
+            m1 += 1
+            i += 1
+        elif A[i] > p:
+            A[m2], A[i] = A[i], A[m2]
+            m2 -= 1
+        else:
+            i += 1
+
+    return m1, m2
 
 
 def test():
