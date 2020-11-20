5bb52636aa3aa5e78f706e2f2314ee6306767422
Hiep <hiepph.2406@gmail.com>
2020-11-20 17:39:25 +0700

    feat: fibonacci implementation

---
+++ week5_dynamic_programming/longest_increasing_subsequence.py
@@ -0,0 +1,17 @@
+T = dict()
+
+
+def lis(A, i):
+    if i not in T:
+        T[i] = 1
+
+    for j in range(i):
+        if A[j] < A[i]:
+            if A[j] < A[i]:
+                T[i] = max(T[i], lis(A, j) + 1)
+    return T[i]
+
+
+if __name__ == '__main__':
+    A = [7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3]
+    print(max(lis(A, i) for i in range(len(A)))
