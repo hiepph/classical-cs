24f66cf86e7467710a81266db511ddfd4f8fbd11
Hiep Pham <hiepph.2406@gmail.com>
2020-11-20 22:45:43 +0700

    feat: longest increasing subsequence implementation and reconstruct the path

--- week5_dynamic_programming/longest_increasing_subsequence.py
+++ week5_dynamic_programming/longest_increasing_subsequence.py
@@ -1,17 +1,50 @@
 T = dict()
 
 
-def lis(A, i):
-    if i not in T:
+def lis(A):
+    T = [None] * len(A)
+
+    for i in range(len(A)):
         T[i] = 1
+        for j in range(i):
+            if A[j] < A[i] and T[i] < T[j] + 1:
+                T[i] = T[j] + 1
 
-    for j in range(i):
-        if A[j] < A[i]:
-            if A[j] < A[i]:
-                T[i] = max(T[i], lis(A, j) + 1)
-    return T[i]
+    return max(T[i] for i in range(len(A)))
 
 
-if __name__ == '__main__':
+def cons_lis(A):
+    """Reconstruct the optimal solution
+    """
+    T = [None] * len(A)
+    prev = [None] * len(A)
+
+    for i in range(len(A)):
+        T[i] = 1
+        prev[i] = -1
+        for j in range(i):
+            if A[j] < A[i] and T[i] < T[j] + 1:
+                T[i] = T[j] + 1
+                prev[i] = j
+
+    # unwinding the solution
+    last = 0
+    for i in range(1, len(A)):
+        if T[i] > T[last]:
+            last = i
+
+    paths = []
+    cur = last
+    while cur >= 0:
+        paths.append(A[cur])
+        cur = prev[cur]
+    paths.reverse()
+    return paths
+
+    return max(T[i] for i in range(len(A)))
+
+
+def test_lis():
     A = [7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3]
-    print(max(lis(A, i) for i in range(len(A)))
+    assert lis(A) == 5
+    assert cons_lis(A) == [2, 3, 4, 6, 9]
