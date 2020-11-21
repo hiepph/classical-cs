13281ce065bb18afdf323cafc514fd6f28cd5983
Hiep Pham <hiepph.2406@gmail.com>
2020-11-21 22:45:31 +0700

    feat(week5): backtrack for primitive calculator

--- week5_dynamic_programming/2_primitive_calculator/primitive_calculator.py
+++ week5_dynamic_programming/2_primitive_calculator/primitive_calculator.py
@@ -20,15 +20,25 @@ def optimal_sequence(n):
     The intermediate numbers that lead to n
     """
     T = [-1] * (n + 1)
+    paths = [-1] * (n + 1)
     for i in reversed(range(1, n + 1)):
         for op in [div3, div2, minus1]:
             v, ok = op(i)
-            if ok and (T[v] < 0 or T[v] > T[i] + 1):
+            if ok and (T[v] < 0 or T[i] + 1 < T[v]):
                 T[v] = T[i] + 1
-    return T[0]
+                paths[v] = i
+
+    # backtrack
+    sequences = []
+    cur = 1
+    while cur != -1:
+        sequences.append(cur)
+        cur = paths[cur]
+    return T[0], sequences
 
 
 def test():
-    assert optimal_sequence(1) == (0, [1])[0]
-    assert optimal_sequence(5) == (3, [1, 2, 4, 5])[0]
-    assert optimal_sequence(96234) == (14, [1, 2, 4, 5])[0]
+    assert optimal_sequence(1) == (0, [1])
+    assert optimal_sequence(5) == (3, [1, 3, 4, 5])
+    assert optimal_sequence(96234) == (
+        14, [1, 3, 9, 10, 11, 33, 99, 297, 891, 2673, 8019, 16038, 16039, 48117, 96234])
