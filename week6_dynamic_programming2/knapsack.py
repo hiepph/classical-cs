6536bc7c0904b360f8f9c258bd4d89853e5e531b
Hiep <hiepph.2406@gmail.com>
2020-11-23 13:41:23 +0700

    week6: knapsack without repetitions

--- week6_dynamic_programming2/knapsack.py
+++ week6_dynamic_programming2/knapsack.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 T = dict()
 
 
@@ -25,3 +27,23 @@ def test_knapsack():
                     v=[30, 14, 16, 9], u=10) == 48  # 30 + 9 + 9
     assert iterative_knapsack(w=[6, 3, 4, 2],
                               v=[30, 14, 16, 9], W=10) == 48  # 30 + 9 + 9
+
+
+def knapsack_without_repetitions(w, v, W):
+    w = [None] + w
+    v = [None] + v
+
+    M = np.zeros((len(w), W + 1))
+    for i in range(1, len(w)):
+        for j in range(1, W + 1):
+            M[i, j] = M[i - 1, j]
+            if w[i] <= j:
+                val = v[i] + M[i - 1, j - w[i]]
+                if M[i, j] < val:
+                    M[i, j] = val
+    return M[-1, -1]
+
+
+def test_knapsack_without_repetitions():
+    assert knapsack_without_repetitions(
+        w=[6, 3, 4, 2], v=[30, 14, 16, 9], W=10) == 46
