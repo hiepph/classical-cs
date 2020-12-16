39ac70239127065a70950993cdd7be702bbf161e
Hiep Pham <hiepph.2406@gmail.com>
2020-12-16 23:37:30 +0700

    week6: placing parentheses

--- week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
+++ week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
@@ -1,4 +1,18 @@
-# Uses python3
+import math
+import re
+import numpy as np
+
+
+OPS = "+-*"
+
+
+def tokenize(s):
+    ds = re.split(r"\+|-|\*", s)
+    ds = list(map(int, ds))
+    ops = re.findall(r"\+|-|\*", s)
+    return ds, ops
+
+
 def evalt(a, b, op):
     if op == '+':
         return a + b
@@ -9,10 +23,39 @@ def evalt(a, b, op):
     else:
         assert False
 
+
+def compute_min_max(m, M, i, j, op):
+    _min = math.inf
+    _max = -math.inf
+
+    for k in range(i, j):
+        a = evalt(M[i, k], M[k + 1, j], op[k])
+        b = evalt(M[i, k], m[k + 1, j], op[k])
+        c = evalt(m[i, k], M[k + 1, j], op[k])
+        d = evalt(m[i, k], m[k + 1, j], op[k])
+        _min = min(_min, a, b, c, d)
+        _max = max(_max, a, b, c, d)
+    return _min, _max
+
+
 def get_maximum_value(dataset):
-    #write your code here
-    return 0
+    d, op = dataset
+    n = len(d)
+    m = np.zeros((n, n))
+    M = np.zeros((n, n))
+
+    for i in range(n):
+        m[i, i] = d[i]
+        M[i, i] = d[i]
+
+    # diagonal moving
+    for s in range(1, n):
+        for i in range(n - s):
+            j = i + s
+            m[i, j], M[i, j] = compute_min_max(m, M, i, j, op)
+    return M[0, n - 1]
 
 
-if __name__ == "__main__":
-    print(get_maximum_value(input()))
+def test():
+    assert get_maximum_value(tokenize("1+5")) == 6
+    assert get_maximum_value(tokenize("5-8+7*4-8+9")) == 200
