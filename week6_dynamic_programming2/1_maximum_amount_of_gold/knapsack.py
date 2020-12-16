c60be5222e5e2ee9d7a4273e90e128c8655e61a4
Hiep Pham <hiepph.2406@gmail.com>
2020-12-16 22:58:01 +0700

    week6: Maximum amount of gold

--- week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -1,15 +1,18 @@
-# Uses python3
-import sys
+T = dict()
+
 
 def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+    if W not in T:
+        T[W] = 0
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    W, n, *w = list(map(int, input.split()))
-    print(optimal_weight(W, w))
+    for i in range(len(w)):
+        if w[i] <= W:
+            v = w[i]
+            remain_w = w.copy()
+            remain_w.pop(i)
+            T[W] = max(T[W], optimal_weight(W - v, remain_w) + v)
+    return T[W]
+
+
+def test():
+    assert optimal_weight(10, [1, 4, 8]) == 9
