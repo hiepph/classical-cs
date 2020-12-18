0873f193154b6eb617a18ec253a72c32885eced0
Hiep Pham <hiepph.2406@gmail.com>
2020-12-18 23:59:27 +0700

    data-structures: check brackets

--- data-structures/week1_basic_data_structures/1_brackets_in_code/check_brackets.py
+++ data-structures/week1_basic_data_structures/1_brackets_in_code/check_brackets.py
@@ -1,6 +1,6 @@
-# python3
-
+import os
 from collections import namedtuple
+
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
@@ -13,19 +13,28 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
+            new_open = Bracket(char=next, position=i + 1)
+            opening_brackets_stack.append(new_open)
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+            new_closed = Bracket(char=next, position=i + 1)
+            if not opening_brackets_stack:
+                return new_closed.position
+            while opening_brackets_stack:
+                top = opening_brackets_stack.pop()
+                if are_matching(top.char, new_closed.char):
+                    break
+                return new_closed.position
+
+    if opening_brackets_stack:
+        return opening_brackets_stack.pop().position
+    return "Success"
 
 
-def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+def test():
+    for i in range(1, len(os.listdir("tests")) // 2 + 1):
+        inp = open("tests/" + str(i).zfill(2)).read().strip()
 
+        res = find_mismatch(inp)
+        target = open("tests/" + str(i).zfill(2) + ".a").read().strip()
 
-if __name__ == "__main__":
-    main()
+        assert str(res) == target, f"test: {i}, input: {inp}"
