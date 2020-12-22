da9ac48a6ee85540ef564758ecdd738d49c8773d
Hiep Pham <hiepph.2406@gmail.com>
2020-12-23 00:18:33 +0700

    data-structures: 2 tree height

--- data-structures/week1_basic_data_structures/2_tree_height/tree-height.py
+++ data-structures/week1_basic_data_structures/2_tree_height/tree-height.py
@@ -1,29 +1,46 @@
-# python3
-
-import sys, threading
-sys.setrecursionlimit(10**7) # max depth of recursion
+from collections import namedtuple
+import sys
+import threading
+sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)  # new thread will get stack of such size
 
-class TreeHeight:
-        def read(self):
-                self.n = int(sys.stdin.readline())
-                self.parent = list(map(int, sys.stdin.readline().split()))
 
-        def compute_height(self):
-                # Replace this code with a faster implementation
-                maxHeight = 0
-                for vertex in range(self.n):
-                        height = 0
-                        i = vertex
-                        while i != -1:
-                                height += 1
-                                i = self.parent[i]
-                        maxHeight = max(maxHeight, height);
-                return maxHeight;
+Node = namedtuple('Node', ['key', 'level'])
+
+
+class TreeHeight:
+    def read(self):
+        self.n = int(sys.stdin.readline())
+        self.parent = list(map(int, sys.stdin.readline().split()))
+
+        # build the tree
+        self.root = -1
+
+        self.nodes = [[] for _ in range(self.n)]
+        for k, parent_id in enumerate(self.parent):
+            if parent_id == -1:
+                self.root = k
+            else:
+                self.nodes[parent_id].append(k)
+        # print(self.nodes)
+
+    def compute_height(self):
+        max_height = 1
+
+        stack = []
+        stack.append(Node(self.root, 1))
+        while stack:
+            node = stack.pop()
+            for child in self.nodes[node.key]:
+                stack.append(Node(child, node.level + 1))
+                max_height = max(max_height, node.level + 1)
+        return max_height
+
 
 def main():
-  tree = TreeHeight()
-  tree.read()
-  print(tree.compute_height())
+    tree = TreeHeight()
+    tree.read()
+    print(tree.compute_height())
+
 
 threading.Thread(target=main).start()
