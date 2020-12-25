51e83f623d1a08734b8bbdc2f46024a9565dd7b5
Hiep Pham <hiepph.2406@gmail.com>
2020-12-26 00:42:03 +0700

    data-structures: max sliding window

--- data-structures/week1_basic_data_structures/5_max_sliding_window/max_sliding_window.py
+++ data-structures/week1_basic_data_structures/5_max_sliding_window/max_sliding_window.py
@@ -1,12 +1,23 @@
-# python3
-
+from collections import deque
 
 def max_sliding_window_naive(sequence, m):
-    maximums = []
-    for i in range(len(sequence) - m + 1):
-        maximums.append(max(sequence[i:i + m]))
+    q = deque()
+    ans = []
+    for i, num in enumerate(sequence):
+        # keep only in sliding window
+        if q and q[0] <= i - m:
+            q.popleft()
 
-    return maximums
+        # keep a decreasing sliding window
+        while q and num >= sequence[q[-1]]:
+            q.pop()
+        q.append(i)
+
+        # get 0-th index for each step
+        ans.append(sequence[q[0]])
+
+    # skip initial values
+    return ans[m-1:]
 
 if __name__ == '__main__':
     n = int(input())
@@ -15,4 +26,3 @@ if __name__ == '__main__':
     window_size = int(input())
 
     print(*max_sliding_window_naive(input_sequence, window_size))
-
