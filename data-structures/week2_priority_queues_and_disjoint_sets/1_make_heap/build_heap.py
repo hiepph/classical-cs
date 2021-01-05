54602c0d0e953eda344369ab32af40b80c51a0ed
Hiep Pham <hiepph.2406@gmail.com>
2021-01-05 23:42:33 +0700

    data-structures: week2/make-heap

--- data-structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
+++ data-structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
@@ -1,24 +1,58 @@
-# python3
+class MinHeap():
+    def __init__(self, init=[]):
+        self.size = len(init)
+        self.__h = init
+        self.swaps = []
 
+    def __str__(self):
+        return str(self.__h)
+
+    def _parent(self, i):
+        if i % 2 == 0:
+            return i // 2 - 1
+        else:
+            return i // 2
+
+    def _left_child(self, i):
+        return 2 * i + 1
+
+    def _right_child(self, i):
+        return 2 * i + 2
+
+    def _swap(self, a, b):
+        self.swaps.append((a, b))
+        self.__h[a], self.__h[b] = self.__h[b], self.__h[a]
+
+    # def _sift_up(self, i):
+    #     while i > 0 and self.__h[self._parent(i)] < self.__h[i]:
+    #         self._swap(i, self._parent(i))
+    #         i = self._parent(i)
+
+    def _sift_down(self, i):
+        max_index = i
+        l = self._left_child(i)
+        if l < self.size and self.__h[l] <= self.__h[max_index]:
+            max_index = l
+        r = self._right_child(i)
+        if r < self.size and self.__h[r] <= self.__h[max_index]:
+            max_index = r
+
+        if i != max_index and max_index < self.size:
+            self._swap(i, max_index)
+            self._sift_down(max_index)
+
+    def build(self):
+        for i in range(self.size // 2, -1, -1):
+            self._sift_down(i)
 
 def build_heap(data):
     """Build a heap from ``data`` inplace.
-
     Returns a sequence of swaps performed by the algorithm.
     """
-    # The following naive implementation just sorts the given sequence
-    # using selection sort algorithm and saves the resulting sequence
-    # of swaps. This turns the given array into a heap, but in the worst
-    # case gives a quadratic number of swaps.
-    #
-    # TODO: replace by a more efficient implementation
-    swaps = []
-    for i in range(len(data)):
-        for j in range(i + 1, len(data)):
-            if data[i] > data[j]:
-                swaps.append((i, j))
-                data[i], data[j] = data[j], data[i]
-    return swaps
+    H = MinHeap(data)
+    H.build()
+
+    return H.swaps
 
 
 def main():
