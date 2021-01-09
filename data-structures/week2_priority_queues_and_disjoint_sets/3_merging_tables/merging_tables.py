2594d79ae0d60becdaf8f950be9150133383689a
Hiep Pham <hiepph.2406@gmail.com>
2021-01-09 21:01:31 +0700

    data-structures: merging tables

--- data-structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
+++ data-structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
@@ -1,6 +1,3 @@
-# python3
-
-
 class Database:
     def __init__(self, row_counts):
         self.row_counts = row_counts
@@ -19,10 +16,22 @@ class Database:
         # merge two components
         # use union by rank heuristic
         # update max_row_count with the new maximum table size
+        if self.ranks[src_parent] > self.ranks[dst_parent]:
+            self.parents[dst_parent] = src_parent
+            self.row_counts[src_parent] += self.row_counts[dst_parent]
+        else:
+            self.parents[src_parent] = dst_parent
+            if self.ranks[src_parent] == self.ranks[dst_parent]:
+                self.ranks[dst_parent] += 1
+            self.row_counts[dst_parent] += self.row_counts[src_parent]
+
+        self.max_row_count = max(self.row_counts)
         return True
 
     def get_parent(self, table):
         # find parent and compress path
+        if table != self.parents[table]:
+            self.parents[table] = self.get_parent(self.parents[table])
         return self.parents[table]
 
 
@@ -33,7 +42,8 @@ def main():
     db = Database(counts)
     for i in range(n_queries):
         dst, src = map(int, input().split())
-        db.merge(dst - 1, src - 1)
+        # db.merge(dst - 1, src - 1)
+        db.merge(src - 1, dst - 1)
         print(db.max_row_count)
 
 
