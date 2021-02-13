9b475a464f6fade248f63a3da500c003ff8cf4c6
Hiep Pham <hiepph.2406@gmail.com>
2021-02-13 23:19:25 +0700

    week3: solve 5 problems

--- data-structures/week3_hash_tables/2_hash_chains/hash_chains.py
+++ data-structures/week3_hash_tables/2_hash_chains/hash_chains.py
@@ -17,7 +17,7 @@ class QueryProcessor:
     def __init__(self, bucket_count):
         self.bucket_count = bucket_count
         # store all strings in one list
-        self.elems = []
+        self.elems = [[] for _ in range(bucket_count)]
 
     def _hash_func(self, s):
         ans = 0
@@ -37,26 +37,25 @@ class QueryProcessor:
     def process_query(self, query):
         if query.type == "check":
             # use reverse order, because we append strings to the end
-            self.write_chain(cur for cur in reversed(self.elems)
-                        if self._hash_func(cur) == query.ind)
+            self.write_chain(reversed(self.elems[query.ind]))
         else:
-            try:
-                ind = self.elems.index(query.s)
-            except ValueError:
-                ind = -1
+            k = self._hash_func(query.s)
+            elem = self.elems[k]
+
             if query.type == 'find':
-                self.write_search_result(ind != -1)
+                self.write_search_result(query.s in elem)
             elif query.type == 'add':
-                if ind == -1:
-                    self.elems.append(query.s)
-            else:
-                if ind != -1:
-                    self.elems.pop(ind)
+                if query.s not in elem:
+                    elem.append(query.s)
+            else:  # del
+                if query.s in elem:
+                    elem.remove(query.s)
 
     def process_queries(self):
         n = int(input())
         for i in range(n):
             self.process_query(self.read_query())
+
 
 if __name__ == '__main__':
     bucket_count = int(input())
