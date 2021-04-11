d00c4564a5a3e1bf9b4af315fb810498f5fe8f2f
Hiep Pham <hiepph.2406@gmail.com>
2021-04-11 23:09:11 +0700

    feat(sort): insertion sort in C

---
+++ sort/insertionsort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void insertion_sort(int arr[], int n)
+{
+  for (int i = 1; i < n; ++i) {
+    // shift the precedent list to the right
+    // insert the current element to the right position
+    // e.g.: 1 3 4 2 ... -> 1 2 3 4 ...
+    int j;
+    int cur =  arr[i];
+    for (j = i-1; j >= 0 && arr[j] > cur; --j)
+      arr[j + 1] = arr[j];
+    arr[j+1] = cur;
+  }
+}
+
+int main()
+{
+  int arr[] = {4, 3, 2, 10, 12, 1, 5, 6};
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  insertion_sort(arr, n);
+  for (int i = 0; i < n; ++i)
+    printf("%d ", arr[i]);
+  printf("\n");
+
+  return 0;
+}
