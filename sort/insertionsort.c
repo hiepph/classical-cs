29f8c3f1eabfaef9423df36c4f3601de653b5d99
Hiep Pham <hiepph.2406@gmail.com>
2021-05-27 11:34:10 +0700

    sort: refactor insertion sort

--- sort/insertionsort.c
+++ sort/insertionsort.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
+// ref: https://www.geeksforgeeks.org/insertion-sort/
 void insertion_sort(int *arr, int n)
 {
+  int i, j;
+  int cur;
+
   for (int i = 1; i < n; ++i) {
+    cur =  arr[i];
     // shift the precedent list to the right
-    // insert the current element to the right position
-    // e.g.: 1 3 4 2 ... -> 1 2 3 4 ...
-    int j;
-    int cur =  arr[i];
-    for (j = i-1; j >= 0 && arr[j] > cur; --j)
+    for (j = i-1; j >= 0 && arr[j] > cur; --j) {
       arr[j + 1] = arr[j];
+    }
+    // insert the current element to the right position
+    // e.g.: 1 3 4 (2) ... -> 1 (2) 3 4 ...
     arr[j+1] = cur;
   }
 }
