358d42eb833b157d64501b7eb69bbbd0b3cada89
Hiep Pham <hiepph.2406@gmail.com>
2022-01-03 18:43:38 +0100

    search: new binary search implementation in C

---
+++ search/binary_search/binary_search.c
@@ -0,0 +1,21 @@
+/*
+ * Iterative version
+ */
+int search(int* nums, int numsSize, int target) {
+  int low = 0;
+  int high = numsSize - 1;
+
+  while (low <= high) {
+    int mid = (low + high) / 2;
+
+    if (nums[mid] == target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+
+  return -1;
+}
