7745aa1378ac7953b021df34b3d2ab6963ecad9d
Hiep Pham <hiepph.2406@gmail.com>
2022-01-19 00:32:10 +0100

    ds(graph): topological sort

---
+++ ds/graph/python/toposort.py
@@ -0,0 +1,46 @@
+from typing import List, Optional
+
+
+def dfs(G: List[List[int]], visited: List[bool],
+        order: List[int], u: int):
+    visited[u] = True
+    for v in G[u]:
+        if not visited[v]:
+            dfs(G, visited, order, v)
+    order.append(u)
+
+
+def toposort(G: List[List[int]]) -> List[int]:
+    """
+
+    Args:
+        G: adjacent list format
+
+    Returns:
+        The vertices sorted topologically
+    """
+    V = len(G)
+
+    visited = [False] * V
+    order = []
+
+    for u in range(V):
+        if not visited[u]:
+            dfs(G, visited, order, u)
+
+    order.reverse()
+    return order
+
+
+def test():
+    #
+    # Graph is represented as an adjacent list
+    # G[u] contains the vertices that u is pointing to
+    #
+    G = [[1, 3, 4],
+         [2],
+         [3],
+         [],
+         [3]]
+
+    assert(toposort(G) == [0, 4, 1, 2, 3])
