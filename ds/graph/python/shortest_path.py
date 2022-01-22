a2e8e9f44d64a767f17630ad2a31fd2485f96f90
Hiep Pham <hiepph.2406@gmail.com>
2022-01-22 23:16:18 +0100

    ds(graph): Bellman Ford algorithm for shortest path with negative weight

--- ds/graph/python/shortest_path.py
+++ ds/graph/python/shortest_path.py
@@ -35,20 +35,23 @@ def dijkstra(edges: List[List[int]], V: int,
     return dist, prev
 
 
-def bellman_ford(G: List[List[Optional[int]]],
+def bellman_ford(edges: List[List[int]], V: int,
                  start: int) -> (List[int], List[Optional[int]]):
-    V = len(G)
-
     dist = [math.inf] * V
     prev = [None] * V
 
     dist[start] = 0
     for _ in range(V - 1):
-        pass
+        for (u, v, weight) in edges:
+            if dist[v] > (dist[u] + weight):
+                dist[v] = dist[u] + weight
+                prev[v] = u
+
+    return dist, prev
 
 
-def test():
-    # non-negative weights
+def test_dijkstra():
+    # edges contain non-negative weights
     edges = [(0, 1, 3),
              (0, 3, 10),
              (1, 2, 3),
@@ -64,4 +67,34 @@ def test():
     assert dist == [0, 3, 6, 9, 7, 7]
     assert prev == [None, 0, 1, 2, 2, 4]
 
-    # negative weights
+
+def test_bellman_ford():
+    # edges contain non-negative weights
+    edges = [(0, 1, 3),
+             (0, 3, 10),
+             (1, 2, 3),
+             (1, 3, 8),
+             (1, 4, 5),
+             (2, 3, 3),
+             (2, 4, 1),
+             (2, 5, 2),
+             (3, 1, 2),
+             (3, 4, 5),
+             (4, 5, 0)]
+    dist, prev = bellman_ford(edges, 6, 0)
+    assert dist == [0, 3, 6, 9, 7, 7]
+    assert prev == [None, 0, 1, 2, 2, 4]
+
+    # edges contain negative weights
+    edges = [(0, 1, 4),
+             (0, 3, 2),
+             (1, 2, 2),
+             (1, 3, 1),
+             (1, 4, 3),
+             (3, 1, 1),
+             (3, 2, 4),
+             (3, 4, 5),
+             (4, 2, -5)]
+    dist, prev = bellman_ford(edges, 5, 0)
+    assert dist == [0, 3, 1, 2, 6]
+    assert prev == [None, 3, 4, 0, 1]
