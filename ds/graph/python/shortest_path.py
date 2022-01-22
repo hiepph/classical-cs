8c20b876b0213d4f9e97405c93a2120b9ec5951b
Hiep Pham <hiepph.2406@gmail.com>
2022-01-22 22:54:20 +0100

    ds(graph): change graph representation in shortest path algorithm

--- ds/graph/python/shortest_path.py
+++ ds/graph/python/shortest_path.py
@@ -3,19 +3,21 @@ import math
 from queue import PriorityQueue
 
 
-def dijkstra(G: List[List[Optional[int]]],
+def dijkstra(edges: List[List[int]], V: int,
              start: int) -> (List[int], List[Optional[int]]):
     """
+    Args:
+        edges: List of (start, end, weight). Vertex index starts from 0.
+        V: number of nodes
+        start: vertice to calculate the distance from
+
     Returns:
         List of distances from current vertice to the start vertice
         List of parent vertices which is the previous visited vertice
         before current vertice
     """
-    V = len(G)
-
     dist = [math.inf] * V
     prev = [None] * V
-
     dist[start] = 0
 
     q = PriorityQueue()
@@ -24,10 +26,7 @@ def dijkstra(G: List[List[Optional[int]]],
 
     while not q.empty():
         _, u = q.get()
-        for v, weight in enumerate(G[u]):
-            if weight is None:
-                continue
-
+        for (u, v, weight) in edges:
             if dist[v] > (dist[u] + weight):
                 dist[v] = dist[u] + weight
                 prev[v] = u
@@ -36,24 +35,32 @@ def dijkstra(G: List[List[Optional[int]]],
     return dist, prev
 
 
-def bellman_ford():
-    pass
+def bellman_ford(G: List[List[Optional[int]]],
+                 start: int) -> (List[int], List[Optional[int]]):
+    V = len(G)
+
+    dist = [math.inf] * V
+    prev = [None] * V
+
+    dist[start] = 0
+    for _ in range(V - 1):
+        pass
 
 
 def test():
-    """Graph is represented as an adjacency matrix where
-    G[u][v] indicates the direction from u -> v and has weight G[u][v].
-
-    G[u][v] == None means no edge.
-    """
     # non-negative weights
-    G = [[None, 3, None, 10, None, None],
-         [None, None, 3, 8, 5, None],
-         [None, None, None, 3, 1, 2],
-         [None, 2, None, None, 5, None],
-         [None, None, None, None, None, 0],
-         [None, None, None, None, None, None]]
-    dist, prev = dijkstra(G, 0)
+    edges = [(0, 1, 3),
+             (0, 3, 10),
+             (1, 2, 3),
+             (1, 3, 8),
+             (1, 4, 5),
+             (2, 3, 3),
+             (2, 4, 1),
+             (2, 5, 2),
+             (3, 1, 2),
+             (3, 4, 5),
+             (4, 5, 0)]
+    dist, prev = dijkstra(edges, 6, 0)
     assert dist == [0, 3, 6, 9, 7, 7]
     assert prev == [None, 0, 1, 2, 2, 4]
 
