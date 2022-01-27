b5520c668cc9b88995adeabc3fa606c88950c83e
Hiep Pham <hiepph.2406@gmail.com>
2022-01-27 12:29:05 +0100

    dynamic: more meaningful variable and clearer docs

--- dynamic/knapsack.py
+++ dynamic/knapsack.py
@@ -1,20 +1,21 @@
-def knapsack(values, weights, W) -> int:
-    """
-    Args:
-        W: knapsack capacity
+def knapsack(values, weights, capacity) -> int:
+    """The implementation is based on the algorithm in wikipedia.
+    Reference: https://www.wikiwand.com/en/Knapsack_problem
 
     Returns:
         Maximum value within the capacity
     """
     n = len(values)
-    cache = [[0 for _ in range(W + 1)]
+    cache = [[0 for _ in range(capacity + 1)]
              for _ in range(n + 1)]
 
     for i in range(1, n + 1):
-        for j in range(W + 1):
+        for j in range(1, capacity + 1):
             if weights[i - 1] > j:
+                # weight cannot fit in this bag
                 cache[i][j] = cache[i - 1][j]
             else:
+                # we either choose this weight or don't choose this weight
                 cache[i][j] = max(
                     cache[i - 1][j],
                     cache[i - 1][j - weights[i - 1]] + values[i - 1])
